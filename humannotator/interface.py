8e9becd6550d9ea8a2ddb3aac7d54a313d8227d7
vanboefer <jenka@protonmail.com>
2019-08-05 16:35:20 +0200

    Set up Interface class

--- humannotator/interface.py
+++ humannotator/interface.py
@@ -1,31 +1,30 @@
+from humannotator.results import Answer, Invalid
+
+
 class Interface(object):
-    def __init__(self):
-        pass
+    def __init__(self, answer):
+        self.answer = answer
+        self._check_input_()
+
+    def _check_input_(self):
+        if not isinstance(self.answer, Answer):
+            raise TypeError(
+                f"`answer` must be of type {Answer}."
+                )
 
     def __call__(self):
-        user = ''
-        while user not in ['+', '-', '?', '.']:
-            # display(HTML(html))
-            user = input(
-                "Please annotate the sample above:\n"
-                "[+] if the sample matches.\n"
-                "[-] if the sample does not.\n"
-                "[?] when unsure.\n"
-                "[.] to exit (current phrase will NOT be saved).\n"
-                )
-            # clear_output()
+        while True:
+            print(f"\n{self.answer.question.instruction}\n{Stop.instruction}\n")
+            user = input()
+            if user == Stop.character:
+                break
 
-        if user == '.':
-            return user
-
-        self.annotations.append(
-            self.Annotation(phrase, row.id, user, datetime.now())
-            )
+            user = self.answer.question(user)
+            if not isinstance(user, Invalid):
+                print(f"Annotation is success! Value is {user}")
+                break
 
 
 class Stop(object):
     character = '.'
-
-
-class Invalid(object):
-    pass
+    instruction = f"[{character}] - exit"
