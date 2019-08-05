53d15d09492b67befb67070dec70138010a384cb
lcvriend <43742775+lcvriend@users.noreply.github.com>
2019-08-06 01:10:44 +0200

    Implement rudimentary display

--- humannotator/display.py
+++ humannotator/display.py
@@ -1,6 +1,40 @@
 # third party
 from IPython.display import HTML, display, clear_output
 
+# local
+from humannotator.utils import Base
 
-class Display(object):
-    pass
+
+def test_for_ipython():
+    try:
+        get_ipython()
+        return True
+    except NameError:
+        return False
+
+
+JUPYTER = test_for_ipython()
+
+
+class Display(Base):
+    line = '=' * 36
+    tab  = ' ' * 4
+
+    def __init__(self, data, instruction):
+        self.data = data
+        self.instruction = instruction
+
+    def __call__(self, id):
+        to_screen = [
+            f"id: {id}",
+            'element:',
+            f"{self.tab}{self.data(id)}",
+            self.line,
+            self.instruction,
+        ]
+        output = '\n'.join(to_screen)
+        print(output)
+
+    @staticmethod
+    def clear():
+        clear_output()
--- humannotator/interface.py
+++ humannotator/interface.py
@@ -1,26 +1,29 @@
 # local
 from humannotator.utils import Base
 from humannotator.annotations import Annotations, Annotation, Invalid
+from humannotator.display import Display
 
 
 class Interface(Base):
-    def __init__(self, annotations):
+    def __init__(self, data, annotations):
+        self.data = data
         self.annotations = annotations
-        self._check_input_('annotations', self.annotations, Annotations)
+        self.instruction = '\n'.join([
+            self.annotations.task.instruction,
+            Stop.instruction,
+            ])
 
     def __call__(self, id):
         annotation = Annotation()
+        display = Display(self.data, self.instruction)
         while True:
-            instruction = '\n'.join(
-                self.annotations.question.instruction,
-                Stop.instruction
-                )
-
+            display(id)
             user = input()
+            display.clear()
             if user == Stop.character:
                 return Stop()
 
-            user = self.annotations.question(user)
+            user = self.annotations.task(user)
             if not isinstance(user, Invalid):
                 return annotation(user)
 
