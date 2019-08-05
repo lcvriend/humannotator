446b03afadf56d957b4ddaf7f3775b8f75ab95c8
vanboefer <jenka@protonmail.com>
2019-08-05 13:24:26 +0200

    Set up Question and Answer classes

--- src/results.py
+++ src/results.py
@@ -1,13 +1,43 @@
+from interface import Invalid, Stop
+
+
 class Question(object):
-    instruction = """
+    def __init__(self, instruction):
+        self.instruction = instruction
 
-    """
+    def __call__(self, value):
+        value = _check_stop_(value)
+        return value
 
-    def __init__(self):
+    def _check_stop_(self, value):
+        if value == Stop.character:
+            return Stop()
+        return value
+
+
+class Question_MultipleChoice(Question):
+    def __init__(self, choices):
+        super().__self__()
+        self.choices = choices
+
+    def __call__(self, value):
+        value = super().__call__(value)
+        if not isinstance(value, Stop) and not value in choices:
+            value = Invalid()
+        return value
+
+
+class Answer(object):
+    def __init__(self, question):
+        self.question = question
         self._value = None
+        self._check_input_()
 
-    def __call__(self, x):
-        self.value = x
+    def _check_input_(self):
+        if not isinstance(self.question, Question):
+            raise TypeError(
+                f"`question` must be of type {Question}."
+                )
 
     @property
     def value(self):
@@ -15,16 +45,5 @@ class Question(object):
 
     @value.setter
     def value(self, value):
-        self._value = value
-
-
-class Question_MultipleChoice(Datum):
-    def __init__(self, choices):
-        super().__self__()
-        self.choices = choices
-
-    @value.setter
-    def value(self, value):
-        if not value in choices:
-            value = None
+        value = self.question(value)
         self._value = value
