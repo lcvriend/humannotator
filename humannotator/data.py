64408cc610638eab7e1487ed2add572f18509882
lcvriend <43742775+lcvriend@users.noreply.github.com>
2019-08-22 15:54:47 +0200

    Rename elements to items

--- humannotator/data.py
+++ humannotator/data.py
@@ -18,7 +18,7 @@ class Data(Base):
             self.data = data
         self._check_input_('data', self.data, self.data_type)
         self.ids = None
-        self.elements = data
+        self.items = data
 
 
 class Data_List(Data):
@@ -27,7 +27,7 @@ class Data_List(Data):
     def __init__(self, data):
         super().__init__(data)
         self.ids = list(range(0, len(data)))
-        self.elements = data
+        self.items = data
 
 
 class Data_Dict(Data):
@@ -36,22 +36,22 @@ class Data_Dict(Data):
     def __init__(self, data):
         super().__init__(data)
         self.ids = data.keys()
-        self.elements = data.values()
+        self.items = data.values()
 
 
 class Data_DataFrame(Data):
     data_type = pd.DataFrame
 
-    def __init__(self, data, element_col, id_col=None):
+    def __init__(self, data, items_col, id_col=None):
         super().__init__(data)
         if id_col:
             self.data = self.data.set_index(id_col)
-        self.element_col = element_col
+        self.items_col = items_col
         self.ids = self.data.index
-        self.elements = self.data[self.element_col]
+        self.items = self.data[self.items_col]
 
     def __getitem__(self, id):
-        return self.data.at[id, self.element_col]
+        return self.data.at[id, self.items_col]
 
 
 if __name__ == '__main__':
