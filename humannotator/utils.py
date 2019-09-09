e7b15b70f07c38dafe920c6a19f192e7dc2c76fb
lcvriend <43742775+lcvriend@users.noreply.github.com>
2019-09-09 14:24:28 +0200

    Move test_for_ipython to utils

--- humannotator/utils.py
+++ humannotator/utils.py
@@ -13,3 +13,14 @@ class Base(object):
 
 def option(character, instruction):
     return f"[{character}] - {instruction}  \n"
+
+
+def test_for_ipython():
+    try:
+        get_ipython()
+        return True
+    except NameError:
+        return False
+
+
+JUPYTER = test_for_ipython()
