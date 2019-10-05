94d891e33218e217fdb8c564cd7e0f0223c46f90
lcvriend <43742775+lcvriend@users.noreply.github.com>
2019-10-05 18:42:55 +0200

    Order variables

--- humannotator/version.py
+++ humannotator/version.py
@@ -1,5 +1,5 @@
 __version__ = '0.0.5'
 __license__ = 'GPLv3+'
 __author__  = 'L.C. Vriend, D.E. Kim'
-__credits__ = ['L.C. Vriend', 'D.E. Kim']
 __email__   = 'vanboefer@gmail.com'
+__credits__ = ['L.C. Vriend', 'D.E. Kim']
