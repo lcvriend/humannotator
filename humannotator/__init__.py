00b8c2d794889f40d449d2f0230ca90054801df2
lcvriend <43742775+lcvriend@users.noreply.github.com>
2019-10-05 18:18:49 +0200

    Update dependencies and version

--- humannotator/__init__.py
+++ humannotator/__init__.py
@@ -1,3 +1,4 @@
 from humannotator.core.tasks import task_factory
 from humannotator.core.data import load_data
 from humannotator.humannotator import Annotator
+from humannotator.version import __version__, __author__
--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
+import re
+from pathlib import Path
 from setuptools import setup, find_packages
+
+PATH = Path(__file__).resolve().parent
+
+def get_version(filename):
+    regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    file = PATH / filename
+    file_content = file.read_text(encoding='utf8')
+    version_match = re.search(regex, file_content, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
     name='humannotator',
-    version='0.0.1',
+    version=get_version('humannotator/version.py'),
     description='Customizable tool for easy manual annotation',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -25,7 +38,7 @@ setup(
     license='GPLv3+',
     packages=find_packages(),
     install_requires=[
-        'pandas',
+        'pandas>=0.24.0',
         'markdown',
     ],
     include_package_data=True,
